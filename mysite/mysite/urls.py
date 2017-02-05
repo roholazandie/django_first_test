aeeec40d8bad553279afd2449bd00c858bef5bef
rohola zandie <hilbert.cantor@gmail.com>
2017-02-06 00:03:13 +0330

    first commit after rescuing files

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,22 @@
+# Embedded file name: /home/rohola/Codes/Python/django_first_project/mysite/mysite/urls.py
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+urlpatterns = [url('^polls/', include('polls.urls')),
+ url('^detail/', include('polls.urls')),
+ url('^init_work/', include('polls.urls')),
+ url('^admin/', admin.site.urls)]
---
+++ mysite/mysite/wsgi.py
@@ -0,0 +1,13 @@
+# Embedded file name: /home/rohola/Codes/Python/django_first_project/mysite/mysite/wsgi.py
+"""
+WSGI config for mysite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+import os
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+application = get_wsgi_application()
